85b3c832104ae7cd93da54048e5f8bff3811a97c
mbnaveen <navinsmb@gmail.com>
2022-07-25 13:40:47 +0530

    commit

--- src/components/separateproduct/seprateProduct.jsx
+++ src/components/separateproduct/seprateProduct.jsx
@@ -13,11 +13,15 @@ const SeprateProduct = () => {
   const [separate,setSeparate]=useState({})
   const [firstImage,setFirstImage]=useState("")
   const [temp,setTemp]=useState("");
+  const [loading,setLoading]=useState(false)
 useEffect(()=>{
    axios.get(`https://mj-back.herokuapp.com/products/product/${id}`).then((res)=>{
     setSeparate(res.data);
     setFirstImage(res.data.img1)
-    
+    setLoading(true)
+    return setTimeout(()=>{
+      setLoading(false);
+},1500)
 }).catch((err)=>{
     console.log(err.message)
 })
@@ -25,6 +29,7 @@ useEffect(()=>{
 },[])
 useEffect(()=>{
   window.scrollTo({top: 0, left: 0, behavior: 'smooth' })
+  
 },[])
 
 const addToCart=()=>{
@@ -34,6 +39,7 @@ return navigate("/login")
   separate["userId"]=userId;
   delete separate["_id"]
   axios.post("https://mj-back.herokuapp.com/cart",separate).then((res)=>{
+   
     dispatch(getProductCart(userId))
     
     setTemp("Product added in bag ")
@@ -45,45 +51,53 @@ return navigate("/login")
   })
 }
   return (
-    <div className='sep-product'>
-    <div>
-<div >
-<img onClick={()=>{
-  setFirstImage(separate.img1)
-}} src={separate.img1} alt="ico" />
-</div>
-<div>
-<img onClick={()=>{
-  setFirstImage(separate.img2)
-}} src={separate.img2} alt="ico" />
-</div>
-    </div>
-    <div>
-      <img src={firstImage} alt="ico" />
-    </div>
-    <div>
-
+    <>
+{
+      loading ? <div className='loading-separate'>
+        <h1>Loading...</h1>
+      </div>  :  <div className='sep-product'>
       <div>
-      <p>
-        {separate.name}
-      </p>
-      <p className='inside-p'>₹{ (separate.price)-((separate.price)*(separate.discount/100))}</p>
-     <p style={{textDecoration:"line-through"}} className='inside-p'>₹{separate.price}</p> 
-      <p className='inside-p'>Offer {separate.discount}%</p>
-      <p className='inside-p'>Size:{separate.size}</p>
-      <span>
-      <p >Color:</p><div style={{backgroundColor:`${separate.color}`}} className='rount-color'></div>
-      </span>
-      
-      
+  <div >
+  <img onClick={()=>{
+    setFirstImage(separate.img1)
+  }} src={separate.img1} alt="ico" />
+  </div>
+  <div>
+  <img onClick={()=>{
+    setFirstImage(separate.img2)
+  }} src={separate.img2} alt="ico" />
+  </div>
       </div>
-      <div className='final-cart-div' >
-      
-      <button onClick={addToCart} className="cart-button">Add to bag</button>
-      <p>{temp}</p>
+      <div>
+        <img src={firstImage} alt="ico" />
       </div>
-    </div>
-    </div>
+      <div>
+  
+        <div>
+        <p>
+          {separate.name}
+        </p>
+        <p className='inside-p'>₹{ (separate.price)-((separate.price)*(separate.discount/100))}</p>
+       <p style={{textDecoration:"line-through"}} className='inside-p'>₹{separate.price}</p> 
+        <p className='inside-p'>Offer {separate.discount}%</p>
+        <p className='inside-p'>Size:{separate.size}</p>
+        <span>
+        <p >Color:</p><div style={{backgroundColor:`${separate.color}`}} className='rount-color'></div>
+        </span>
+        
+        
+        </div>
+        <div className='final-cart-div' >
+        
+        <button onClick={addToCart} className="cart-button">Add to bag</button>
+        <p>{temp}</p>
+        </div>
+      </div>
+      </div>
+    }
+   
+    </>
+    
   )
 }
 
