57b780d66e53efac0118a040998cf6e8d646c7dc
mbnaveen <navinsmb@gmail.com>
2022-07-25 13:50:32 +0530

    commit

--- src/components/categories/show.jsx
+++ src/components/categories/show.jsx
@@ -1,14 +1,21 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./main.css"
 import { useNavigate } from 'react-router-dom'
 const Show = ({products}) => {
-  
+  const [loading,setLoading]=useState(true)
   const navigate=useNavigate();
 useEffect(()=>{
   window.scrollTo({top: 0, left: 0, behavior: 'smooth' })
+  setTimeout(()=>{
+    setLoading(false)
+  },1500)
 },[])
   return (
     <>
+{
+loading ? <div className='loading-separate'>
+  <h1>Loading...</h1>
+</div> :   <>
     {
       products.map((p)=>{
         return <div>
@@ -32,6 +39,10 @@ onMouseLeave={(e)=>{
       })
     }
     </>
+}
+    </>
+    
+ 
   )
 }
 
